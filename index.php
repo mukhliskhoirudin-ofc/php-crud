0f0ceb742a8ce86f8f97e64f1cc5039a9e63420d
mukhliskhoirudin-ofc <mukhliskhoirudin.official@gmail.com>
2025-01-03 09:08:37 +0700

    feat: implement read functionality to display student data

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+require 'functions.php';
+
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Gambar</th>
+            <th>NPM</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+            <th>Aksi</th>
+        </tr>
+
+        <?php $i = 1; ?>
+        <?php foreach ($mahasiswa as $mhs): ?>
+            <tr>
+                <td><?= $i++ ?></td>
+                <td><img src="img/<?= $mhs["gambar"]; ?>" width="50"></td>
+                <td><?= $mhs["npm"]; ?></td>
+                <td><?= $mhs["nama"]; ?></td>
+                <td><?= $mhs["email"]; ?></td>
+                <td><?= $mhs["jurusan"]; ?></td>
+                <td>
+                    <a href="">update</a> |
+                    <a href="">delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+
+</html>

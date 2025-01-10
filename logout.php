a2d78a3c58450b791a73e90adaa9a4ac3c589556
mukhliskhoirudin-ofc <mukhliskhoirudin.official@gmail.com>
2025-01-10 07:40:33 +0700

    feat: implement cookie removal in logout process

--- logout.php
+++ logout.php
@@ -5,5 +5,8 @@ $_SESSION = []; //tambahan biar yakin
 session_unset(); // Hapus semua data session
 session_destroy(); // Hancurkan session
 
+setcookie('id', '', time() - 3600);
+setcookie('key', '', time() - 3600);
+
 header("Location: login.php"); // Redirect ke halaman login
 exit();
